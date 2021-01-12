a4c11731f389313f167d0964d9b2981a4719b894
Sirichai Janpan <sirichai.jan@ascendcorp.com>
2021-01-12 17:06:33 +0700

    initial project

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TicketController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+        return 1;
+    }
+}
